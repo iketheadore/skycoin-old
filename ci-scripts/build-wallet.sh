eede56c4552a544f449617202777f843a1f450d0
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-11-25 00:38:28 +1100

    test

--- ci-scripts/build-wallet.sh
+++ ci-scripts/build-wallet.sh
@@ -8,9 +8,10 @@ fi
 
 echo "start to build wallets..."
 pushd "electron" >/dev/null
-echo "os name: $OS_NAME"
-if [[ "$OS_NAME" == "Linux" ]]; then ./build.sh 'linux/amd64 linux/arm' ;fi
-if [[ "$OS_NAME" == "macOS" ]]; then ./build.sh 'darwin/amd64' ;fi
-if [[ "$OS_NAME" == "Windows" ]];  then ./build.sh 'windows/amd64 windows/386'; fi
+#echo "os name: $OS_NAME"
+#if [[ "$OS_NAME" == "Linux" ]]; then ./build.sh 'linux/amd64 linux/arm' ;fi
+if [[ "$OS_NAME" == "macOS" ]]; then ./build.sh ;fi
+#if [[ "$OS_NAME" == "macOS" ]]; then ./build.sh 'darwin/amd64' ;fi
+#if [[ "$OS_NAME" == "Windows" ]];  then ./build.sh 'windows/amd64 windows/386'; fi
 ls release/
 popd >/dev/null
